50c0a5fc4915d1756877e5273951ea1f02bf00b4
Shingirai98 <https://github.com/Shingirai98>
2021-07-17 23:33:56 +0200

    [ADDED] recursive lamda function and numpy sum function

--- digital_sum.py
+++ digital_sum.py
@@ -1,24 +1,18 @@
+import numpy as np
 
 # recursive functiuon to calculate n!
 def factorial(n):
   if (n != 1):
     ans = (n)*factorial(n-1)
-  
   else:
     ans = 1
-  
   return ans
 
-# def sum():
-#   return 
+def summing(arr):
+  arr2 = []
+  for i in arr:
+    arr2.append(int(i))
+  return np.sum(np.array(arr2))
 
 fact = (lambda x: 1 if x == 0 else x* fact(x-1))
-
-sum = 0
-#print(list(str(fact(5))))
-#print((lambda y: (for digit in str(fact): sum+=eval(digit)))
-
-for digit in str(str(fact(5))):
-  sum += eval(digit) 
-  
-print(sum)
+print(summing(list(str(fact(5)))))
